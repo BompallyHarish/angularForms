69ef5fcb5f2e85be5a609b28195692e01431d8be
Harish Bompally <harishb@zenwork.com>
2024-06-10 14:24:44 +0530

    basic changes.

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; 
+import { Component, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { User } from './user';
+import { Signup } from './signup';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() { }
+  public data: any
   title = 'tdf';
   public topics = ["Angular", "NodeJS", "MongoDB", "Express"]
-public userModel = new User("Harish","test@gmail.com","9502315173","Main Road", "Gandhari","",true,"morning")
+  public userModel = new User("Harish", "test@gmail.com", "9502315173", "Main Road", "Gandhari", "", true, "morning")
+  public signUpModel = new Signup("", "", "", "")
+  @ViewChild('userForm') userForm!: NgForm;
+
+  onSignup() {
+    console.log(this.userModel)
+  }
+  onSubmit() {
+    console.log(this.userModel)
+
+    this.userForm.reset()
+  }
+
 }
---
+++ tdf/src/app/signup.ts
@@ -0,0 +1,9 @@
+export class Signup {
+    constructor(
+        public contactName: String,
+        public password: String,
+        public email: string,
+        public phoneNumber: String
+    ) {
+    }
+}
